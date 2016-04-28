055154a19ce42d138957b8f4836f51427b55b85e
Edward Yoo <ehyoo97@gmail.com>
2016-04-28 14:02:56 -0400

    destroy votes let's try this again

---
+++ app/controllers/post_votes_controller.rb
@@ -0,0 +1,30 @@
+class PostVotesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  def upvote
+  end
+
+  def downvote
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+
+end
--- test/controllers/post_votes_controller_test.rb
+++ test/controllers/post_votes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UpvoteTest < ActiveSupport::TestCase
+class PostVotesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
