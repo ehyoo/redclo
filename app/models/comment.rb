f482de84d704d9bbc8dd370c8cfa70b34bb0417e
Edward Yoo <ehyoo97@gmail.com>
2016-04-29 08:08:03 -0400

    ordering posts but it's still ugly

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   belongs_to :op_comment, :class_name => "Comment"
-  has_many :replies, :class_name => "Comment"
+  has_many :replies, :class_name => "Comment", dependent: :destroy
   has_many :cvotes
 end
